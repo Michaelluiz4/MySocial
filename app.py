fbfd2e05b0b54709dd12a1974365f7440d94b502
Michael <michaelsb065@gmail.com>
2025-11-17 00:53:16 -0300

    criando as rotas e testando o funcionamento

--- app.py
+++ app.py
@@ -1,7 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "hello"
+    return render_template("index.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    ...
+
+
+@app.route("/create_account")
+def create_account():
+    ...
+
+
+@app.route("/following")
+def following():
+    ...
+
+
+@app.route("/profile")
+def profile():
+    ...
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
